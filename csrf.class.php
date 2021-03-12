1f14ab0b569cdd8231f4fe0492d301aff78795c8
KaplanOmr <43470074+KaplanOmr@users.noreply.github.com>
2021-03-12 22:48:38 +0300

    C2 - Project Basic Completed

--- csrf.class.php
+++ csrf.class.php
@@ -1,31 +1,92 @@
-<?php 
-class CSRF {
+<?php
+class CSRF
+{
+    private $csrfList;
+    protected $inputName;
 
-    public function __construct($expiredToken = false, $exprideTokenTime = 3000)
-    {   
+    public function __construct(string $inputName = '_csrf', bool $expiredToken = false, int $exprideTokenTime = 3000)
+    {
 
-        if(!isset($_SESSION['csrf'])){
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+
+        if (!isset($_SESSION['csrf'])) {
             $_SESSION['csrf'] = [];
         }
 
 
-        if($expiredToken){
+        if ($expiredToken) {
 
             $expTime = time() - $exprideTokenTime;
 
             $tmpArr = $_SESSION['csrf'];
 
             foreach ($tmpArr as $index => $csrf) {
-                if($csrf['created'] > $expTime){
+                if ($csrf['created'] > $expTime) {
                     unset($_SESSION['csrf'][$index]);
                 }
             }
         }
-        
+
+
+        $this->csrfList = $_SESSION['csrf'];
+        $this->inputName = $inputName;
     }
 
     public function createToken(): string
     {
-        return '';
+        $tokenData = rand() . "-" . time();
+        $token = hash('sha256', $tokenData);
+
+        array_push($_SESSION['csrf'], [
+            'token' => $token,
+            'time' => time()
+        ]);
+
+        return $token;
+    }
+
+    public function createInput(): string
+    {
+        $token = $this->createToken();
+        $returnStr = '<input type="hidden" name="' . $this->inputName . '" value="' . $token . '">';
+        return $returnStr;
+    }
+
+    public function checkToken(string $token): bool
+    {
+        if ($this->checkCsrfList()) {
+            foreach ($this->csrfList as $index => $csrf) {
+                if (hash_equals($token, $csrf['token'])) {
+                    unset($_SESSION['csrf'][$index]);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function checkRequest(array $request): bool
+    {
+        if ($this->checkCsrfList()) {
+            if (isset($request[$this->inputName]) && !empty($request[$this->inputName])) {
+                $check = $this->checkToken($request[$this->inputName]);
+                if ($check) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private function checkCsrfList(): bool
+    {
+        if (count($this->csrfList)) {
+            return true;
+        }
+
+        return false;
     }
 }
--- example.php
+++ example.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSRF Class Example</title>
 </head>
-<body>
 
-    <?php 
+<body>
+    <h1>CSRF Example</h1>
+    <hr>
+
+    <?php
+    require_once('csrf.class.php');
+
+    $csrf = new CSRF();
+    $csrfInput = $csrf->createInput();
+
+    if($_REQUEST){
+        if($csrf->checkToken($_GET['_csrf'])){
+            echo '<span style="color:green">Success</span><hr>';
+        }else{
+            echo '<span style="color:red">Failed</span><hr>';
+        }
+    }
 
     ?>
 
+    <form action="" method="get">
+        <?= $csrfInput ?>
+        <input type="text" name="name">
+        <br>
+        <br>
+        <input type="submit" value="send">
+    </form>
 
-    
+
+
 </body>
+
 </html>
